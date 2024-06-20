3510ec5394067e65fd30267cd5a64884a8d8c601
johannesstroebele91 <stroebele.j@gmail.com>
2024-06-20 08:24:39 +0200

    Add component to add and delete habits with service

---
+++ src/app/components/habit.component.ts
@@ -0,0 +1,164 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from "@angular/core";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatCheckbox, MatCheckboxChange} from "@angular/material/checkbox";
+import {NgForOf, NgIf} from "@angular/common";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatButton, MatIconButton, MatMiniFabButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatList, MatListItem} from "@angular/material/list";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatLineModule} from "@angular/material/core";
+import {Habit, UserWithHabits} from "../shared/models";
+import {UsersHabitsService} from "../services/users-habits.service";
+import {BehaviorSubject, Subscription} from "rxjs";
+
+
+@Component({
+  selector: 'app-habit',
+  template: `
+    <mat-card style="max-width: 700px; min-width: 400px; min-height: 575px;">
+      <mat-card-header style="margin: 0 auto 20px auto">
+        <mat-card-title style="font-size: 30px; margin-top: 20px;">My Habits</mat-card-title>
+      </mat-card-header>
+
+      <mat-card-content style="width: 100%">
+        <form [formGroup]="habitForm">
+          <div style="margin: 0 20px; display: inline-block;">
+            <button (click)="addHabit()" mat-mini-fab color="primary" aria-label="Add a habit">
+              <mat-icon>add</mat-icon>
+            </button>
+          </div>
+          <mat-form-field style="width: 70%;">
+            <input matInput formControlName="newHabit" placeholder="Enter new habit">
+          </mat-form-field>
+        </form>
+
+        <mat-list *ngFor="let habit of userWithHabits?.habits">
+          <div *ngIf="!habit.editing" style="display: flex; align-items: center;">
+            <mat-checkbox [ngModel]="habit.checked" (change)="markHabitAsCompleted(habit, $event)"
+                          style="margin-left: 20px">
+            </mat-checkbox>
+            <mat-label style="margin-left: 20px; width: 60%">{{ habit.name }}</mat-label>
+            <button mat-icon-button (click)="editHabit(habit)" style="margin-left: 5px" aria-label="Edit habit">
+              <mat-icon>edit</mat-icon>
+            </button>
+          </div>
+
+          <div *ngIf="habit.editing">
+            <div style="margin: 0 20px; display: inline-block;">
+              <button (click)="saveHabit(habit)" mat-mini-fab color="primary" aria-label="save habit">
+                <mat-icon>save</mat-icon>
+              </button>
+            </div>
+            <mat-form-field style="width: 70%">
+              <input matInput [(ngModel)]="habit.name" placeholder="Save habit" (keyup.enter)="saveHabit(habit)"
+              >
+            </mat-form-field>
+          </div>
+        </mat-list>
+      </mat-card-content>
+    </mat-card>
+  `,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    MatCheckbox,
+    NgForOf,
+    MatLineModule,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatIcon,
+    MatListItem,
+    MatList,
+    MatIconButton,
+    MatDividerModule,
+    FormsModule,
+    MatMiniFabButton,
+    NgIf
+  ],
+  standalone: true
+})
+export class HabitComponent implements OnInit, OnDestroy {
+  @Input() userWithHabits!: UserWithHabits;
+  @Output() pointsAddedByHabitCompletion = new EventEmitter<number>();
+  habitForm = new FormGroup({
+    newHabit: new FormControl('')
+  });
+
+  private userWithHabitsSubject: BehaviorSubject<UserWithHabits>;
+  private userWithHabitsSubscription: Subscription | undefined;
+
+  constructor(private usersHabitsService: UsersHabitsService) {
+    this.userWithHabitsSubject = new BehaviorSubject<UserWithHabits>(this.userWithHabits);
+  }
+
+  ngOnInit() {
+    this.userWithHabitsSubject.next(this.userWithHabits); // Initialize with the input data
+
+    // Subscribe to changes in userWithHabits
+    this.userWithHabitsSubscription = this.userWithHabitsSubject.subscribe(userWithHabits => {
+      if (userWithHabits) {
+        this.userWithHabits = userWithHabits;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.userWithHabitsSubscription?.unsubscribe();
+  }
+
+  addHabit() {
+    const newHabitName = this.habitForm.value.newHabit;
+    if (newHabitName) {
+      const newHabit: Habit = {name: newHabitName, checked: false, editing: false}
+      if (this.userWithHabits?.habits?.length > 0) {
+        this.userWithHabits?.habits?.unshift(newHabit);
+      } else {
+        this.userWithHabits = {...this.userWithHabits, habits: [newHabit]}
+      }
+      this.updateUserWithHabits();
+    }
+    this.habitForm.reset();
+  }
+
+  markHabitAsCompleted(habit: Habit, event: MatCheckboxChange) {
+    if (event.checked) {
+      const index = this.userWithHabits.habits.indexOf(habit);
+      if (index > -1) {
+        this.userWithHabits.habits.splice(index, 1);
+        this.updateUserWithHabits();
+        this.pointsAddedByHabitCompletion.emit(50)
+      }
+    }
+  }
+
+  editHabit(habit: Habit) {
+    habit.editing = true;
+    this.updateUserWithHabits();
+  }
+
+  saveHabit(habit: Habit) {
+    habit.editing = false;
+    this.updateUserWithHabits();
+  }
+
+  updateUserWithHabits() {
+    this.userWithHabitsSubject.next(this.userWithHabits); // Emit updated userWithHabits
+    this.usersHabitsService.updateUser(this.userWithHabits).subscribe({
+      next: (response: any) => {
+        console.log("User habits updated successfully", response);
+      },
+      error: (error: any) => {
+        console.error('Error updating user habits:', error);
+      }
+    });
+  }
+}
---
+++ src/app/services/users-habits.service.ts
@@ -0,0 +1,47 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from "./auth.service";
+import {Habit, UserWithHabits} from "../shared/models";
+import {Observable} from "rxjs";
+
+interface ResponseData {
+  [key: string]: UserWithHabits;
+}
+
+const DOMAIN =
+  'https://habitoid-987-default-rtdb.europe-west1.firebasedatabase.app/';
+const USERS_WITH_HABITS_PATH = '/usersWithHabits';
+
+@Injectable({
+  providedIn: 'root', // This makes AuthService available throughout the application
+})
+export class UsersHabitsService {
+  authService = inject(AuthService);
+  http = inject(HttpClient);
+
+  createUserWithDefaultHabits(id: string, name: string): Observable<{ name: string }> {
+    const habits: Habit[] = [
+      {name: 'Add your example habit above', checked: false},
+      {name: 'Click on the checkbox to mark a habit as complete', checked: false},
+    ];
+
+    const userWithHabits: UserWithHabits = {
+      id: id,
+      name: name,
+      habits: habits,
+    }
+
+    return this.http.put<{
+      name: string;
+    }>(`${DOMAIN}${USERS_WITH_HABITS_PATH}/${id}.json`, userWithHabits);
+  }
+
+  getUser(id: string): Observable<ResponseData> {
+    return this.http.get<ResponseData>(`${DOMAIN}${USERS_WITH_HABITS_PATH}/${id}.json`)
+  }
+
+
+  updateUser(userWithHabits: UserWithHabits): Observable<any> {
+    return this.http.put(`${DOMAIN}${USERS_WITH_HABITS_PATH}/${userWithHabits.id}.json`, userWithHabits);
+  }
+}
